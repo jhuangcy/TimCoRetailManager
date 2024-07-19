02e10daa21a9fcdf13ab6332b45246224e2fd3c6
jhuangcy <jcyh87@gmail.com>
2024-07-19 11:17:58 -0600

    Add wpf dependency injection

--- TimCoRetailManager_WPF/Bootstrapper.cs
+++ TimCoRetailManager_WPF/Bootstrapper.cs
@@ -11,6 +11,9 @@ namespace TimCoRetailManager_WPF
 {
     public class Bootstrapper : BootstrapperBase
     {
+        // Setup dependency injection
+        SimpleContainer container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
@@ -19,6 +22,18 @@ namespace TimCoRetailManager_WPF
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key) => container.GetInstance(service, key);
+        protected override IEnumerable<object> GetAllInstances(Type service) => container.GetAllInstances(service);
+        protected override void BuildUp(object instance) => container.BuildUp(instance);
+        protected override void Configure()
+        {
+            container.Instance(container);
+            container.Singleton<IWindowManager, WindowManager>().Singleton<IEventAggregator, EventAggregator>();
+
+            // Link views to view models
+            GetType().Assembly.GetTypes().Where(t => t.IsClass && t.Name.EndsWith("ViewModel")).ToList().ForEach(v => container.RegisterPerRequest(v, v.ToString(), v));
         }
     }
 }
